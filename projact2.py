be22a1075e61f344dc12ce36ccf551afb92fa1bd
Hamza Abbas <amula1115@gmail.com>
2026-07-23 17:12:14 +0500

    Update projact2.py

--- projact2.py
+++ projact2.py
@@ -1,1 +1,12 @@
+a = int(input("Enter the number:"))
+b = int(input("Enter the number:"))
+#this is variable to store value!C
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"/",b,"=",a/b)
+print(a,"//",b,"=",a//b)
+print(a,"**",b,"=",a**b)
+print(a,"%",b,"=",a%b)
+print(a,"|",b,"=",a|b)
+print(a,"^",b,"=",a^b)
 
