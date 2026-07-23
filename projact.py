f2674be504309c200f37de45e6abfcc2a69cd136
Hamza Abbas <amula1115@gmail.com>
2026-07-23 17:10:02 +0500

    Update projact.py

--- projact.py
+++ projact.py
@@ -1,1 +1,19 @@
+a = 333
+b = 777
+ans = a+b
+print("addtion of",a,"and",b,"is",ans)
+ans1 = a-b
+print("subtraction of",a,"and",b,"is",ans1)
+ans2 = a*b
+print("multiplication of",a,"and",b,"is",ans2)
+ans3 = a//b
+print("division of",a,"and",b,"is",ans3)
+ans4 = a%b
+print("modulo of",a,"and",b,"is",ans4)
+ans5 = a**b
+print("exponentiation of",a,"and",b,"is",ans5)
+ans6 = a/b
+print("remainder of",a,"and",b,"is",ans6)
+ans7 = a^b
+print("remainder of",a,"and",b,"is",ans7)
 
